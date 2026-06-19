82541773b96447528007bff3f57535617d87ba97
Julian <seminarioreyes2021@gmail.com>
2026-06-19 12:00:30 -0500

    Add new features

---
+++ src/app.js
@@ -0,0 +1,4 @@
+
+// punto de entrada de la aplicación que va a tener dependencias a los demás módulos, requisitos de otros archivos, etc.
+const { emailTemplate } = require('./js-foundation/01-template');
+console.log(emailTemplate);
---
+++ src/js-foundation/01-template.js
@@ -0,0 +1,9 @@
+const emailTemplate = `
+<div>
+    <h1>Gracias por registrarte en nuestro sitio web. Estamos emocionados de tenerte como parte de nuestra comunidad.</h1>
+    <p>¡Disfruta de tu experiencia en nuestro sitio web!, hi</p>
+    </div>`;
+
+module.exports = {
+    emailTemplate
+}   
