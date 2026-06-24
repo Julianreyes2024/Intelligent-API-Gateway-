89b557f1c245dcd57e2054c77587c27f9f96a235
Julian <seminarioreyes2021@gmail.com>
2026-06-24 13:13:21 -0500

    promesas y peticiones

--- src/app.js
+++
@@ -1,9 +0,0 @@
-
-// punto de entrada de la aplicación que va a tener dependencias a los demás módulos, requisitos de otros archivos, etc.
-const { emailTemplate } = require('./js-foundation/01-template');/* sintaxis tradicional de importaciones en nodejs*/
-
-//console.log(emailTemplate);
-const { emailTemplate } = require('./js-foundation/02-destructuring');
-const { emailTemplate } = require('./js-foundation/03-callbacks');
-
-
--- src/js-foundation/03-callbacks.js
+++
@@ -1,5 +0,0 @@
-const users = [//los callbacks Son fundamentales para manejar operaciones asincrónicas en JavaScript, como solicitudes de red, temporizadores o eventos del usuario.
-  { name: 'Alice', age: 30 },
-  { name: 'Bob', age: 25 },
-  { name: 'Charlie', age: 35 }
-];
